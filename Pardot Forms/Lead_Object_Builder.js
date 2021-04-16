3b2f1a99e0e6cb6c45d0e2717fb8d738bbd118dc
Cufee <vkouzin@outlook.com>
2021-04-16 12:30:18 -0400

    Added Pardot redirect method

---
+++ Pardot Forms/Lead_Object_Builder.js
@@ -0,0 +1,83 @@
+// Get Lead object from form fields
+function getLeadObject() {
+  // Loop through all form elements and map
+  var data = {
+    CPTenantDomain: tenantDomain,
+    CPTenantRouter: tenantRouter,
+  };
+  var labelsDict = {};
+  // Save label names
+  form.querySelectorAll("label").forEach((label) => {
+    labelsDict[label.htmlFor] = stripText(label.textContent);
+  });
+  // Fill lead object
+  for (var i = 0, elem; (elem = form.elements[i++]); ) {
+    if (elem.type.includes("submit") || elem.type.includes("fieldset")) {
+      continue;
+    } else if (elem.type.includes("select")) {
+      // Get the field name from label or element text
+      var field_name = (n = stripText(elem.options[0].text)) ? n : labelsDict[elem.name];
+      if (!field_name) {
+        if (typeof debugMessages !== "undefined" ? debugMessages : true) {
+          console.log("failed to find a valid field name for " + elem.name);
+        }
+        continue;
+      }
+      // Save to lead obj
+      if (elem.selectedIndex === 0) {
+        data[field_name] = "[not provided]";
+      } else {
+        data[field_name] = elem.options[elem.selectedIndex].text;
+      }
+    } else if (elem.parentElement.className.includes("hidden")) {
+      // Get the field name from class name
+      var field_classes = elem.parentElement.className.split(" ");
+      var field_name;
+      for (var i in field_classes) {
+        if (field_classes[i].includes("CP_")) {
+          field_name = field_classes[i];
+        }
+      }
+      if (!field_name) {
+        if (typeof debugMessages !== "undefined" ? debugMessages : true) {
+          console.log("failed to find a valid field name for " + elem.name);
+        }
+        continue;
+      }
+      // Save to lead obj
+      data[field_name] = (v = elem.value) ? v : "[not provided]";
+    } else {
+      // Get the field name from label or element placeholder
+      var field_name = (n = stripText(elem.placeholder)) ? n : labelsDict[elem.name];
+      if (!field_name) {
+        if (typeof debugMessages !== "undefined" ? debugMessages : true) {
+          console.log("failed to find a valid field name for " + elem.name);
+        }
+        continue;
+      }
+      // Save to lead obj
+      if (elem.type.includes("checkbox")) {
+        data[field_name] = (v = elem.checked) ? v : false;
+      } else {
+        data[field_name] = (v = elem.value) ? v : "[not provided]";
+      }
+    }
+  }
+  if (typeof debugMessages !== "undefined" ? debugMessages : true) {
+    // Log data and labels for debugging
+    console.log(labelsDict);
+    console.log(data);
+  }
+  return data;
+}
+// Strip characters and spaces
+function stripText(text) {
+  return camelText(text).replace(/[^\w]/gi, "");
+}
+// Make text more readable
+function camelText(str) {
+  return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match, index) {
+    if (+match === 0) return "";
+    return match.toUpperCase();
+  });
+}
--- Pardot Forms/Native Pardot page/readme.md
+++ Pardot Forms/Native Pardot page/readme.md
@@ -1,3 +1,9 @@
 This method should be used with native Pardot pages.
 
 How this works:
+#TODO
+
+How to set this up:
+1. Add the code from Step_1.html to the **Look & Feel → Below Form Code Editor**
+2. Add the code from Step_3.html to the **Parent Webpage** in which the Pardot form code resides or to the **Thank you page** if a redirect is enabled.
+3. Map the values from the submitData object to your Chili Piper Form which is mapped to your Router
